b7010bd7bb89b21f4160d32f4c3f9a8b515ec446
Mian56 <ahmad.amy560@gmail.com>
2025-11-13 02:05:38 -0500

    1

---
+++ fitbuddy/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <div>
+            <h1>FitBuddy</h1>
+            <p>Welcome to your fitness accountability dashboard.</p>
+        </div>
+    );
+}
+
+export default App;
